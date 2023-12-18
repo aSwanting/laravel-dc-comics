ae43eec23f3f4602417e3345850bb481960740dc
aSwanting <gab.damico@gmail.com>
2023-12-18 17:21:19 +0100

    add: Comic model, seeder, migration, controller, etc

--- database/migrations/2023_12_18_153335_create_comics_table.php
+++ database/migrations/2023_12_18_153335_create_comics_table.php
@@ -11,13 +11,15 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('comics', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('title');
+            $table->text('description');
+            $table->text('thumb');
+            $table->unsignedDecimal('price');
+            $table->string('series');
+            $table->date('sale_date');
+            $table->string('type');
             $table->timestamps();
         });
     }
@@ -27,6 +29,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('comics');
     }
 };

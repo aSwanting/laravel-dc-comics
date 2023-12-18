9ccf822c34aaaf094e0ae420bf0fc7e6c6458381
aSwanting <gab.damico@gmail.com>
2023-12-18 18:58:43 +0100

    ADD - create / store

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Comic;
 use App\Http\Requests\StoreComicRequest;
 use App\Http\Requests\UpdateComicRequest;
+use Illuminate\Http\Request;
 
 class ComicController extends Controller
 {
@@ -12,6 +13,26 @@ class ComicController extends Controller
     {
         $comics = Comic::all();
         return view('comics.index', compact('comics'));
+    }
+
+    public function create()
+    {
+        return view('comics.create');
+    }
+
+    public function store(Request $request)
+    {
+        $data = $request->all();
+        $new_comic = new Comic();
+        $new_comic->title = $data['title'];
+        $new_comic->description = $data['description'];
+        $new_comic->thumb = $data['thumb'];
+        $new_comic->price = str_replace('$', '', $data['price']);
+        $new_comic->series = $data['series'];
+        $new_comic->sale_date = $data['sale_date'];
+        $new_comic->type = $data['type'];
+        $new_comic->save();
+        return redirect()->route('comics.show', $new_comic);
     }
 
     public function show(Comic $comic)
---
+++ resources/views/comics/store.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('title', 'Comic Store')
+
+@section('content')
+    <div class="container">
+    </div>
+@endsection
